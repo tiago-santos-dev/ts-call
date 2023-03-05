3e532e65c5c950361477dfc698375a8d7b71ff7f
Tiago Santos <trodrigues322@gmail.com>
2023-03-04 22:49:54 -0300

    style: eslint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { Heading } from '@ignite-ui/react'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <Heading as='h1'>Hello World</Heading>
+  return <Heading as="h1">Hello World</Heading>
 }
