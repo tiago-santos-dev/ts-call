ed8eb4fd926f3ed9c084b868d5e8152c10a71e33
Tiago Santos <trodrigues322@gmail.com>
2023-03-04 22:36:12 -0300

    feat: add design system, global css and external font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { Heading } from '@ignite-ui/react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <h1>Hello World</h1>
+  return <Heading as='h1'>Hello World</Heading>
 }
