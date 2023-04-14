eeec7b73d14bc6676028181164fdb59ac56f5bfb
Tiago Santos <trodrigues322@gmail.com>
2023-04-14 08:26:53 -0300

    feat: :sparkles: add connect calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,43 @@
+import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
+import { useRouter } from "next/router";
+import { ArrowRight } from "phosphor-react";
+import { Container, Header } from "../styles";
+import { ConnectBox, ConnectItem } from "./styles";
+
+export default function Register() {
+
+  const router = useRouter();
+
+  async function handleRegister() {
+
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as='strong'>Conecte sua agenda</Heading>
+        <Text>
+          Conecte o seu calendário para verificarmos automaticamente os
+          horários disponíveis e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+
+        <ConnectBox>
+          <ConnectItem>
+            <Text>Google Calendar</Text>
+            <Button variant='secondary' size='sm'>
+              Conectar
+              <ArrowRight />
+            </Button>
+          </ConnectItem>
+
+          <Button type="submit">
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </ConnectBox>
+
+      </Header>
+    </Container>
+  )
+}
